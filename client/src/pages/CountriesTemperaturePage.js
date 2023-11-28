fa6f0bb5da7d614e5ad81fe533296082fee124d6
Irene Tang <irenetang14@gmail.com>
2023-11-27 18:55:07 -0800

    added chart to temps

--- client/src/pages/CountriesTemperaturePage.js
+++ client/src/pages/CountriesTemperaturePage.js
@@ -1,18 +1,14 @@
-
-import { useEffect, useState } from 'react';
-import { Button, Checkbox, Container, FormControlLabel, Grid, Link, Slider, TextField } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Button, Container, Grid, Slider } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
-import LazyTable from '../components/LazyTable';
+import Chart from 'chart.js/auto'; // Import Chart.js
 
 const config = require('../config.json');
 
 export default function CountriesTemperaturePage() {
-    const flexFormat = { display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'space-evenly' };
-
     const [pageSize, setPageSize] = useState(5);
     const [data, setData] = useState([]);
     const [selectedCountry, setSelectedCountry] = useState(null);
-
     const [temperatureChange, setTemperatureChange] = useState([-2, 2]);
 
     const search = () => {
@@ -24,16 +20,48 @@ export default function CountriesTemperaturePage() {
             });
     };
 
+    const displayChart = () => {
+        fetch(`http://${config.server_host}:${config.server_port}/country/temperature?delta_low=${temperatureChange[0]}&delta_high=${temperatureChange[1]}`)
+            .then(res => res.json())
+            .then(resJson => {
+                const countriesWithId = resJson.map((country) => ({ id: country.Country, ...country }));
+
+                // Chart creation logic within the search function
+                const ctx = document.getElementById('temperatureChangeChart');
+                const labels = countriesWithId.map(country => country.Country);
+                const tempChangeValues = countriesWithId.map(country => country.TemperatureChange);
+
+                const chart = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: labels,
+                        datasets: [{
+                            label: 'Temperature Change',
+                            data: tempChangeValues,
+                            backgroundColor: 'rgba(255, 99, 132, 0.6)',
+                            borderColor: 'rgba(255, 99, 132, 1)',
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
+            });
+    };
 
     const columns = [
         { field: 'Country', headerName: 'Country', width: 100 },
         { field: 'TemperatureChange', headerName: 'TemperatureChange', width: 200 },
     ];
 
-
     return (
         <Container>
-            <h2>View Countries by Temperture Change 2006-2022</h2>
+            <h2>View Countries by Temperature Change 2006-2022</h2>
             <Grid container spacing={6}>
                 <Grid item xs={15}>
                     <p>Temperature Change</p>
@@ -60,6 +88,17 @@ export default function CountriesTemperaturePage() {
                 autoHeight
                 onSelectionModelChange={(selection) => setSelectedCountry(selection[0])}
             />
+
+            <br></br>
+            <br></br>
+
+            <h2>Chart</h2>
+
+            <Button onClick={() => displayChart()} style={{ marginTop: '16px' }} variant="contained" color="primary" >
+                Display Chart
+            </Button>
+
+            <canvas id="temperatureChangeChart" width="400" height="200"></canvas>
 
         </Container>
     );

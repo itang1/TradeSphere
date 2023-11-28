544a14f8cf439edb460de9c8dca66dc0df1eb044
Irene Tang <irenetang14@gmail.com>
2023-11-27 18:28:16 -0800

    formatting

--- client/src/pages/TradesPage.js
+++ client/src/pages/TradesPage.js
@@ -59,22 +59,22 @@ export default function TradingPage() {
     const [categories, setCategories] = useState('')
 
     const [data,setData] = useState([])
-   
-    
+
+
     useEffect(() => {
       fetch(`http://${config.server_host}:${config.server_port}/trading_volume`)
         .then(res => res.json())
         .then(vol => setVolume(vol))
         .catch(error => console.error(error))
-        
+
     }, []);
     console.log(volume)
-  
+
     const searchVol = () => {
       fetch(`http://${config.server_host}:${config.server_port}/trading_volume?type=${type}`
       )
         .then(res => res.json())
-        .then(vol => setVolume(vol))      
+        .then(vol => setVolume(vol))
         .catch(error => console.error(error))
     }
     console.log(volume)
@@ -84,10 +84,10 @@ export default function TradingPage() {
           .then(res => res.json())
           .then(resJson => setTraidD(resJson))
           .catch(error => console.error(error));
-          
+
       }, []);
       console.log(traidd)
-    
+
     const searchTraidD = () => {
         fetch(`http://${config.server_host}:${config.server_port}/trading_data?type=${type}` +
         `&category=${categories}`)
@@ -102,30 +102,30 @@ export default function TradingPage() {
           .then(res => res.json())
           .then(resJson => setTraidP(resJson))
           .catch(error => console.error(error));
-          
+
       }, []);
       console.log(traidp)
-    
+
       const searchTraidP = () => {
         fetch(`http://${config.server_host}:${config.server_port}/trading_partner_catg?type=${type}` +
         `&country2=${countries2}`)
           .then(res => res.json())
           .then(resJson => setTraidP(resJson))
           .catch(error => console.error(error))
-          
+
       }
       console.log(traidp)
- 
+
 
      useEffect(() => {
         fetch(`http://${config.server_host}:${config.server_port}/trading_partner`)
          .then(res => res.json())
          .then(resJson => setTraidPC(resJson))
          .catch(error => console.error(error));
-          
+
      }, []);
      console.log(traidpc)
-    
+
     const searchTraidPC = () => {
         fetch(`http://${config.server_host}:${config.server_port}/trading_partner?type=${type}` +
         `&category=${categories}`)
@@ -135,7 +135,7 @@ export default function TradingPage() {
         }
         console.log(traidpc)
         console.log(data)
-     
+
     const columnsVol = [
       { field: 'CountryName', headerName: 'Country', flex:1},
       { field: 'Continent', headerName: 'Continent', flex:1},
@@ -164,23 +164,23 @@ export default function TradingPage() {
                   //const [position, setPosition] = useState(null)
                       if(traidpc.Country2 === markers.name) {
                           console.log('match')
-                          console.log(markers[0].position);         
+                          console.log(markers[0].position);
                         return (
                           <Marker position = {markers[0].position} icon={customMarkerIcon} title = {markers[0].name}>
-                            
+
                           </Marker>
                         )}
 
                         else {return (<Marker position = {[54, 15]} icon={customMarkerIcon}>
-                            
+
                           </Marker>
                         )}
-                        
+
                         }
-                  
+
         const pos2 = [-37.699450, 176.279420]
-            
-return (  
+
+return (
       <Container>
         <h2>Largest US trading partner in a specificoi7 category </h2>
 
@@ -204,7 +204,7 @@ return (
         <InputLabel variant="standard" htmlFor="categories">
         Select a Category
        </InputLabel>
-        <Select id = "categories" 
+        <Select id = "categories"
         sx={{width: 200,height: 50,
       }} onChange =
         {(event) => {setCategories(event.target.value)}} value = {categories} >
@@ -218,7 +218,7 @@ return (
 
         <Button onClick={() => searchTraidPC() } style={{ left: '10%', transform: 'translateX(-90%)' }}>
           Search
-        </Button> 
+        </Button>
 
         <DataGrid
           rows={traidpc}
@@ -231,19 +231,19 @@ return (
         />
 
         <MapContainer center={[54, 15]} zoom={5.5}scrollWheelZoom={false } style={{ height: '50vh', width: '50wh' } } id="map"
-        > 
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" 
+        >
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
         integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossOrigin="" />
-        
-        <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" 
+
+        <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
         integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossOrigin=""></script>
-        
+
         <TileLayer
         attribution= '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
       <NewMarker/>
-        
+
           {/*<Marker position= {pos2} icon={customMarkerIcon}>
       <Popup>
         A pretty CSS3 popup. <br /> Easily customizable.
@@ -273,7 +273,7 @@ return (
 
         <Button onClick={() => searchVol() } style={{ left: '10%', transform: 'translateX(-90%)' }}>
           Search
-          </Button> 
+          </Button>
           <h2>Results</h2>
           <DataGrid
           rows={volume}
@@ -307,7 +307,7 @@ return (
         <InputLabel variant="standard" htmlFor="categories">
         Select a Category
        </InputLabel>
-        <Select id = "categories" 
+        <Select id = "categories"
         sx={{width: 200,height: 50,
       }} onChange =
         {(event) => {setCategories(event.target.value)}} value = {categories} >
@@ -317,8 +317,8 @@ return (
         <MenuItem value="Rail Locomotives">Rail Locomotives</MenuItem>
         </Select>
           </Grid>
-         
-        
+
+
           <Button onClick={() => searchTraidD() } style={{ left: '10%', transform: 'translateX(-90%)' }}>
           Search
           </Button>
@@ -332,10 +332,10 @@ return (
           onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
           autoHeight
         />
-          
+
         <h4>Interpretation of results</h4>
         <h2>Largest trading category with the US </h2>
-       
+
         <Grid item xs={2} >
        <InputLabel variant="standard" htmlFor="types">
         Select a Trade Type
@@ -366,10 +366,10 @@ return (
         <MenuItem value="Canada">Canada</MenuItem>
         </Select>
           </Grid>
-         
+
           <Button onClick={() => searchTraidP() } style={{ left: '50%', transform: 'translateX(-50%)' }}>
           Search
-          </Button> 
+          </Button>
           <h2>Results</h2>
 
           <DataGrid
@@ -383,7 +383,7 @@ return (
         />
         <h4>Interpretation of results</h4>
 
-        
+
       </Container>
     );
 }

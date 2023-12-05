d2172e0ffce137cb7b59779f8992e6fc48a3163c
zue3103 <82049693+zue3103@users.noreply.github.com>
2023-12-04 21:56:05 -0500

    Update HomePage.js
    adding text and graph for route 8

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,42 +1,108 @@
-import React, { useEffect, useState } from 'react';
-import LazyTable from '../components/LazyTable';
-import {Container} from '@mui/material';
+import { useEffect, useState } from 'react';
+import {  Container, MenuItem , Grid} from '@mui/material';
+import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts'
+//import { LineChart } from '@mui/x-charts/LineChart';
+
 
 
 const config = require('../config.json');
 
-export default function HomePage() {
-  const [author, setAuthor] = useState('');
+export default function TradingPage() {
 
-  useEffect(() => {
-    fetch(`http://${config.server_host}:${config.server_port}/author/name`)
-      .then(res => res.text())
-      .then(restText => setAuthor(restText));
+    const [author, setAuthor] = useState('')
 
-  }, []);
+    const [expor, setExpor] = useState([])
+    const [labour, setLabour] = useState([])
+    const [pageSize, setPageSize] = useState(10);
+    const [data, setData] = useState([])
+    const [water,setWater] = useState([])
+
+      
+    useEffect(() => {
+        fetch(`http://${config.server_host}:${config.server_port}/author/name`)
+          .then(res => res.text())
+          .then(resText => setAuthor(resText));
+
+        fetch(`http://${config.server_host}:${config.server_port}/trading_export`)
+          .then(data=> data.json())
+          .then((data) => {setExpor(data) ;
+          })
+        
+        .catch(error => console.error(error));
+       }, []);   
+       console.log(expor)  
 
 
+      useEffect(() => {
 
-  const tradeColumns = [
+        fetch(`http://${config.server_host}:${config.server_port}/labour`)
+          .then(data=> data.json())
+          .then((data) => {setLabour(data) ;
+          })
+        
+        .catch(error => console.error(error));
+       }, []);   
+       console.log(labour)  
+
+       useEffect(() => {
+       fetch(`http://${config.server_host}:${config.server_port}/populationwater`)
+          .then(data=> data.json())
+          .then((data) => {setWater(data) ;
+          })
+        
+        .catch(error => console.error(error));
+       }, []);   
+       console.log(labour) 
+  
+      useEffect(() => {
+        fetch(`http://${config.server_host}:${config.server_port}/distcountries`)
+          .then(res => res.json())
+          .then(resJson => setData(resJson))
+          .catch(error => console.error(error));
+      }, []);
+
+      const menuItems = data.map(item => (
+        <MenuItem value={item.Country2}>{item.Country2}</MenuItem>
+      ));
+
+    const traidEColumns = [
     {
       field: 'Category',
       headerName: 'Category',
+      flex:1
     },
     {
       field: 'TotalExportValue',
-      headerName: 'Total Export Value '
-    },
+      headerName: 'Total Export Value ',
+      flex:1
+    }
   ];
 
-  return (
+  const labourColumns = [
+    {
+      field: 'HighestUnemploymentRate',
+      headerName: 'HighestUnemploymentRate',
+      flex:1
+    },
+    {
+      field: 'Year',
+      headerName: 'Year',
+      flex:1
+    }
+  ];
+
+return (
     <Container>
-      <h1>PURPOSE OF WEBSITE AND DATABSE </h1>
+    <h1>Trading Economics Web Application</h1>
+    <h3>Purpose and Usage</h3>
       <p>This web application provides an interactive platform for visualizing statistics pertaining to
-        the United States' domestic trading patterns of the United States. It also displays
-        additional characteristics, such as population size, on its trading partners.
+        the United States' trading patterns and 
+        additional characteristics, such as demographics and social-economic indicators, on its trading partners.
         <br></br>
         <br></br>
-        Users can explore detailed analytical findings in detail tin the Countries and Trades pages.
+        Users can explore detailed analytical findings on trade-related and country-specific indicators on the 
+        Trades and Countries pages respectively.
         They can interactively customize their data exploration by selecting countries,
         specific time intervals, and the type of exchanged goods by using our
         user-friendly tools (including sliders, drop-down menus, and pagination).
@@ -45,28 +111,50 @@ export default function HomePage() {
         We anticipate that our web application appeals to researchers and business
         owners who are interested in import and export strategies, market selection,
         and supply chain optimization.</p>
-
-      <br></br>
-      <hr></hr>
-
-      <h1>US TRADING EXPORT</h1>
-
-      <p>The table below displays all of the material categories that the United States
-        exports, along with their total dollar value in USD.
+      
+      <h3>Data Introduction and Preview </h3>
+      <p>The data used in this application stems from a SQL database which contains United States' trading information from 
+        2005 to 2022 and country specific information. More information on the data sources used for the database can be found here
+      <a href="https://tradingeconomics.com"> Trading Data</a> and here <a href="https://ourworldindata.org/hunger-and-undernourishment"> Country Data. </a> 
+       Due to the vast number of trading information only some aspects were chosen to be showcased on this website, such as the total number
+       of exports conducted by the United States, as can be seen in this table, and the continents with the highest proportion of 
+       unimproved drinking water.  
       </p>
 
-      <h2>All Exports Information</h2>
-      <LazyTable route={`http://${config.server_host}:${config.server_port}/trading_export`}
-        columns={tradeColumns}
-        defaultPageSize={10}
-        rowsPerPageOptions={[10, 25, 50, 100]}
-      />
-
-      <br></br>
-      <br></br>
-      <br></br>
-
+      <Grid item xs={2} > 
+      <h3>Unimproved Drinking Water by Continent</h3>
+            <BarChart width={730} height={250} data={water}>
+              <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="Continent" />
+                <YAxis />
+                <Tooltip />
+                <Bar dataKey="MaxUnimprovedWater" fill="#8884d8" />
+  
+              </BarChart> 
+              <h3>Total Export by Trade Category</h3>
+      <DataGrid
+          rows={expor}
+          columns={traidEColumns}
+          pageSize={pageSize}
+          getRowId={(data) => data.Category}
+          rowsPerPageOptions={[5, 10, 25]}
+          onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+          autoHeight
+          components={{ Toolbar: GridToolbar }}
+          componentsProps={{
+            toolbar: {
+              csvOptions: { disableToolbarButton: false},
+              printOptions: { disableToolbarButton: true },
+              showQuickFilter: true,
+              quickFilterProps: { debounceMs: 250 },
+            },}}
+          
+      /> </Grid>
+      
       <p>{author}</p>
+ 
     </Container>
-  );
-};
+);
+
+}
+
